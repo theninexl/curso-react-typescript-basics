84c39c3a732fbc3e1ec5fc50e5631e33d1fabf9c
theninexl <thenine@thenine.es>
2023-12-28 11:41:57 +0100

    change in RandomFox component to receive the image as a strict typed prop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { RandomFox } from "@/components/RandomFox";
 
+//generate random number between 1 and 123
+const random = () => Math.floor(Math.random()*123)+1;
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Hello World</h1>
-      <RandomFox />
+      <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`} />
     </main>
   )
 }
