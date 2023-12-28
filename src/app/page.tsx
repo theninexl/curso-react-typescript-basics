65494e0be0f8ce9959c4c9dcbfa8b26d76488b58
theninexl <thenine@thenine.es>
2023-12-28 12:22:44 +0100

    refactoring of state to use objects and apply the correct type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,28 @@
 import { useState } from "react";
 import { RandomFox } from "@/components/RandomFox";
 
+//generate simple unique id
+const generateId = () => Math.random().toString(36).substring(2,9);
+
 //generate random number between 1 and 123
 const random = () => Math.floor(Math.random()*123)+1;
 
+type ImageItems = { id: string, url: string };
+
 export default function Home() {
-  const [images, setImages] = useState<string[]>([
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
+  const [images, setImages] = useState<Array<ImageItems>>([
+    {id:generateId(), url: `https://randomfox.ca/images/${random()}.jpg`},
+    {id:generateId(), url: `https://randomfox.ca/images/${random()}.jpg`},
+    {id:generateId(), url: `https://randomfox.ca/images/${random()}.jpg`},
+    {id:generateId(), url: `https://randomfox.ca/images/${random()}.jpg`},
   ]);
 
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Hello World</h1>
-      {images.map((image,index) => (
-        <div key={index} className='p-4'>
-          <RandomFox image={image} />
+      {images.map(({ id, url }) => (
+        <div key={id} className='p-4'>
+          <RandomFox image={url} />
         </div>
       ))}
     </main>
