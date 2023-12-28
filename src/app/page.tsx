1ee36a0705a53fd57e36a9475b7b0ba8d00cf930
theninexl <thenine@thenine.es>
2023-12-28 11:59:46 +0100

    add a react state hook correctly typed to be explicit in typescript

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
+'use client';
+import { useState } from "react";
 import { RandomFox } from "@/components/RandomFox";
 
 //generate random number between 1 and 123
 const random = () => Math.floor(Math.random()*123)+1;
 
 export default function Home() {
+  const [images, setImages] = useState<string[]>([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+  ]);
+
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Hello World</h1>
-      <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`} />
+      {images.map((image,index) => (
+        <div key={index} className='p-4'>
+          <RandomFox image={image} />
+        </div>
+      ))}
     </main>
   )
 }
